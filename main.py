f3a44f053bee8032535cc9ed04ae358db709a370
MISday <47630005+MISday@users.noreply.github.com>
2023-08-04 18:09:36 +0800

    parser

---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def make_parser():
+    parser = argparse.ArgumentParser("Genshin Fishing")
+    parser.add_argument("--mode", default="genshin", type=str, help="train or test")
+    return parser
+
+if __name__ == "__main__":
+    args = make_parser().parse_args()
+    print(args.mode)
+    pass
