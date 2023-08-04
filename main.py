ee49ab87e85be6cc43770ae40672400f142b06b3
MISday <47630005+MISday@users.noreply.github.com>
2023-08-04 18:10:25 +0800

    format

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def make_parser():
     parser.add_argument("--mode", default="genshin", type=str, help="train or test")
     return parser
 
+
 if __name__ == "__main__":
     args = make_parser().parse_args()
     print(args.mode)
